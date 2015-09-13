3cddcd80aebf2759a2e915f75529347ede53e1fb
Ambar Sethi <ambarrrsethi@gmail.com>
2015-09-13 18:28:00 +0100

    customer and product routes working fully

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,51 @@
 class ProductsController < ApplicationController
 
   #index
+  def index
+    @products = Product.all 
+  end
+
   #new
+  def new 
+    @product = Product.new
+  end
   #create
+  def create 
+    @product =  Product.new(params.require(:product).permit(:name, :description, :price, :quantity))
+    # binding.pry
+    if @product.save
+      flash[:notice] = 'Product created successfully!'
+      redirect_to @product
+    else
+      render :new
+    end
+  end
   #show
+  def show
+    @product = Product.find(params[:id])
+  end
+
   #edit
+  def edit 
+    @product = Product.find(params[:id])
+  end
   #update
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(params.require(:product).permit(:name, :description, :price, :quantity))
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
   #destroy
+  def destroy
+    product = Product.find(params[:id]).destroy
+
+    redirect_to products_path
+  end
 
   
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,52 @@ class ReviewsController < ApplicationController
 
 
   #index
+  def index
+    @reviews = Review.all
+  end
+
   #new
+  def new 
+    @review = Review.new
+  end
+
   #create
+  def create 
+    @review =  Review.new(params.require(:review).permit(:content, :rating, :recommend))
+
+    if @review.save
+      flash[:notice] = 'review created successfully!'
+      redirect_to @review
+    else
+      render :new
+    end
+  end
+
   #show
-  #edit
+  def show
+    @review = Review.find(params[:id])
+  end
+
+ #edit
+  def edit 
+    @review = Review.find(params[:id])
+  end
   #update
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update(params.require(:review).permit(:name, :description, :price, :quantity))
+      redirect_to @review
+    else
+      render :edit
+    end
+  end
+
   #destroy
+  def destroy
+    review = Review.find(params[:id]).destroy
 
-
+    redirect_to reviews_path
+  end
 
 end
