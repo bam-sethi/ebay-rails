3f8f491fadf7f17cf20a5ffca4609a0ddc04545b
Ambar Sethi <ambarrrsethi@gmail.com>
2015-09-13 16:20:30 +0100

    customers routes and views working

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,20 +7,46 @@ class CustomersController < ApplicationController
 
   #new
   def new
-    customer = Customer.new
+    @customer = Customer.new
   end
 
   #create
   def create
-    @customer = Customer.new(customer_params)
+    customer = Customer.new(params.require(:customer).permit(:name, :age, :nationality))
+    if customer.save
+      flash[:notice] = 'New Customer added!'
+      redirect_to customer
+    else
+      render :new
+    end
   end
 
-
   #show
-  #edit
-  #update
-  #destroy
+  def show 
+    @customer = Customer.find(params[:id])
+  end
 
+  #edit
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  #update
+  def update 
+    customer = Customer.find(params[:id])
+
+    if customer.update(params.require(:customer).permit(:name, :age, :nationality))
+      redirect_to customer
+    else
+      render :edit
+    end
+  end
+
+  #destroy
+  def destroy
+    customer = Customer.find(params[:id]).destroy
+      redirect_to customers_path
+  end
 
 
 end
